4c19c03c235517c7d52c4b013e05d83acad9d170
anhtt2211 <trananh22112001@gmail.com>
2023-01-01 22:10:05 +0700

    wip

--- src/features/article/components/TagList.tsx
+++ src/features/article/components/TagList.tsx
@@ -4,7 +4,7 @@ export function TagList({ tagList }: { tagList: string[] }) {
       {tagList.map((tag) => (
         <li
           key={tag}
-          className="p-1 mr-1 text-center font-light border-solid border-1 border-[#ddd] rounded-xl inline-block"
+          className="p-1 mb-1 mr-1 text-center font-light border-solid border-1 border-[#ddd] rounded-xl inline-block"
         >
           {tag}
         </li>
